25cb0d3de9e28a165e7438e14aef17872b331a7a
Tarun Sabarinath <tarunsabarinath.09@gmail.com>
2021-10-08 18:16:38 +0530

    created contraint bodies

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint ;
+ 
+
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constrainedLog,chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -33,11 +37,13 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    constrainedLog = new Log(200,200,80,PI/2);
+    bird = new Bird (200,200) ;
 
-    bird = new Bird(100,100);
+   chain = new Chain (bird.body,constrainedLog.body) ;
+    
 
 }
-
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
@@ -58,7 +64,14 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    constrainedLog.display();
 
     bird.display();
     platform.display();
+   
+  chain.display() ;  
+
+
+
+
 }
